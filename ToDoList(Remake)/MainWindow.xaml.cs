6087b6703e09f8c4872c7b221cd2c477e9f8d668
KYAN <tvquan12345@gmail.com>
2025-10-05 00:09:06 +1000

    Cleaning codes

--- ToDoList(Remake)/MainWindow.xaml.cs
+++ ToDoList(Remake)/MainWindow.xaml.cs
@@ -1,20 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ToDoList_Remake_.Services;
 
 namespace ToDoList_Remake_
@@ -92,7 +79,6 @@ namespace ToDoList_Remake_
 
         private void FilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // Добавляем проверку на null
             if (todos != null && FilterComboBox.SelectedItem is ComboBoxItem selectedItem)
             {
                 string filter = selectedItem.Tag?.ToString();
